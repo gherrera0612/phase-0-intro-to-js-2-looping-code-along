3c0ce1495e6317f2d6cb469d99c94c86bcfe108d
Trey Herrera <therrera152@gmail.com>
2022-02-28 13:25:11 -0600

    commit tree b8527d79cfdad24683dec188aed5f07df7cb8ef1

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function writeCards (names,toDo){
+    let thankyou = []
+    for (let i =0; i < names.length; i++){
+        thankyou.push( `Thank you, ${names[i]}, for the wonderful ${toDo} gift!` )
+      }
+return thankyou
+}
+//returns an array of thankyou message for each name provided
+//assigns thank you to an array
+//names run through loop
+//.push add the extra elemenets to create a sentence
+
+function countDown (start){
+    while (start >0){
+        console.log(start);
+        start -=1;
+    }
+    console.log(start);
+}
